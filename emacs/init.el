81e6ec96465b0dcc685e3f5936065d13f33d7def
ForNeVeR <neverthness@gmail.com>
2013-09-02 22:41:21 +0700

    Add desktop improvements.

--- emacs/init.el
+++ emacs/init.el
@@ -10,6 +10,17 @@
 (add-to-list 'process-coding-system-alist
              '("powershell.exe" . (cp866-dos . cp866-dos)))
 
+;; Automatically save and restore sessions:
+(setq desktop-dirname             "~/.emacs.d/desktop/"
+      desktop-base-file-name      "emacs.desktop"
+      desktop-base-lock-name      "lock"
+      desktop-path                (list desktop-dirname)
+      desktop-save                t
+      desktop-files-not-to-save   "^$" ;reload tramp paths
+      desktop-load-locked-desktop nil)
+(desktop-save-mode 1)
+(desktop-read)
+
 ;; Fix the shift-selection broken by the starter kit:
 (global-unset-key (kbd "S-<left>"))
 (global-unset-key (kbd "S-<right>"))
