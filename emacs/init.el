1a46b668efb5d479002df67e8419ba3b27205ad9
ForNeVeR <neverthness@gmail.com>
2013-09-05 21:30:16 +0700

    Use gruber-darker theme.

--- emacs/init.el
+++ emacs/init.el
@@ -2,7 +2,7 @@
 
 (server-start)
 
-(setq my-packages '(batch-mode multiple-cursors paredit powershell))
+(setq my-packages '(batch-mode gruber-darker-theme multiple-cursors paredit powershell))
 (loop for p in my-packages
       when (not (package-installed-p p))
       do (package-install p))
@@ -44,7 +44,7 @@
 (global-set-key (kbd "M-C-<up>") 'mc/mark-previous-like-this)
 (global-set-key (kbd "M-C-<down>") 'mc/mark-next-like-this)
 
-(load-theme 'tsdh-dark)
+(load-theme 'gruber-darker)
 (setq default-file-name-coding-system 'cp1251)
 (setq file-name-coding-system 'cp1251)
 
