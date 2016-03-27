7c347ea1fb68422e1244476a03340f1b209742e4
Friedrich von Never <friedrich@fornever.me>
2016-03-27 23:07:32 +0700

    Install: set the schematics up

---
+++ Install.ps1
@@ -0,0 +1,13 @@
+# TODO: Install Chocolatey
+# TODO: Install ConEmu
+# TODO: Install Far Manager
+# TODO: Install Atom
+
+# TODO: Install fonts
+# TODO: Set up ConEmu (use fonts)
+# TODO: Set up PowerShell profile
+# TODO: Set up PS-Agnoster in PowerShell
+# TODO: Create shortcut to run Far Manager through ConEmu
+# TODO: Set up Far Manager + ConEmu plugins (Background.xml)
+# TODO: Set up Atom (~/atom)
+# TODO: Set up Atom as Far editor
