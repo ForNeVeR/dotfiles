e072c47cf1feeb7925bb02350c05fb9c0a138071
Friedrich von Never <friedrich@fornever.me>
2016-03-30 14:01:57 +0700

    vim: add vim config and setup

--- Install.ps1
+++ Install.ps1
@@ -1,7 +1,9 @@
+$ErrorActionPreference = 'Stop'
 # TODO: Install Chocolatey
 # TODO: Install ConEmu
 # TODO: Install Far Manager
 # TODO: Install Atom
+# TODO: Install vim
 
 # TODO: Install fonts
 # TODO: Set up ConEmu (use fonts)
@@ -11,3 +13,26 @@
 # TODO: Set up Far Manager + ConEmu plugins (Background.xml)
 # TODO: Set up Atom (~/atom)
 # TODO: Set up Atom as Far editor
+
+function SafeResolve-Path ($path) {
+    $ExecutionContext.SessionState.Path.GetUnresolvedProviderPathFromPSPath($path)
+}
+
+function New-FileLink ($source, $destination) {
+    cmd /c mklink $(SafeResolve-Path $destination) $(Resolve-Path $source)
+}
+
+function New-DirectoryLink ($source, $destination) {
+    cmd /c mklink /j $(SafeResolve-Path $destination) $(Resolve-Path $source)
+}
+
+# vim setup
+if (-not (Test-Path '~/.vimrc')) {
+    Write-Output 'Creating a link to .vimrc'
+    New-FileLink (Join-Path $PSScriptRoot 'vim/.vimrc') '~/.vimrc'
+}
+
+if (-not (Test-Path '~/vimfiles')) {
+    Write-Output 'Creating a link to vimfiles'
+    New-DirectoryLink (Join-Path $PSScriptRoot 'vim/vimfiles') '~/vimfiles' 
+}
---
+++ vim/.vimrc
@@ -0,0 +1,8 @@
+set backspace=indent,eol,start
+set encoding=utf-8
+set expandtab
+set tabstop=4
+
+call plug#begin('~/.vim/plugged')
+Plug 'PProvost/vim-ps1'
+call plug#end()
