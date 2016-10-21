c08ed2f92a9b423ada4df35c163b52a8f9eaba27
Friedrich von Never <friedrich@fornever.me>
2016-10-21 11:07:17 +0700

    aria2server: add a script

---
+++ Scripts/aria2server.ps1
@@ -0,0 +1,40 @@
+param (
+    $SecretFile = '~/.aria2cserver-secret.txt'
+)
+
+$ErrorActionPreference = 'Stop'
+
+function Generate-Secret {
+    function Generate-Diapason($first, $last) {
+        [int][char]$first..[int][char]$last | % { [string][char]$_ }
+    }
+
+    $alphabet = (Generate-Diapason 'a' 'z') +
+        (Generate-Diapason 'A' 'Z') +
+        (Generate-Diapason '0' '9')
+
+    $length = 128
+
+    $provider = New-Object Security.Cryptography.RNGCryptoServiceProvider
+    try {
+        [byte[]]$bytes = @(0) * $length
+        $provider.GetBytes($bytes)
+        $letters = $bytes | % { $alphabet[$_ % $alphabet.Length] }
+        $letters -join ''
+    } finally {
+        $provider.Dispose()
+    }
+}
+
+if (Test-Path $SecretFile) {
+    Write-Output "Loading secret from $SecretFile..."
+    $secret = Get-Content $SecretFile -Encoding utf8
+} else {
+    Write-Output "Generating aria2c secret file in $SecretFile..."
+    $secret = Generate-Secret
+    Write-Output "Secret generated is $secret"
+
+    $secret | Out-File $SecretFile -Encoding utf8
+}
+
+aria2c --enable-rpc --rpc-allow-origin-all=true --summary-interval=0 --rpc-secret=$secret
